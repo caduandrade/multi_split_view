1e62beea411cb42a456d4e01a88436084d4b5165
cadu <caduandr@tecgraf.puc-rio.br>
2021-10-11 20:59:59 -0300

    Grooved divider (style 2). (#9)

--- lib/src/divider_painter.dart
+++ lib/src/divider_painter.dart
@@ -52,11 +52,13 @@ class DividerPainters {
       Color? highlightedBackgroundColor,
       bool animationEnabled = DividerPainter.defaultAnimationEnabled,
       Color color = _GroovedDividerPainter1.defaultColor,
-      Color? highlightedColor,
+      Color? highlightedColor = _GroovedDividerPainter1.defaultHighlightedColor,
       StrokeCap strokeCap = _GroovedDividerPainter1.defaultStrokeCap,
       double thickness = _GroovedDividerPainter1.defaultThickness,
-      double? highlightedThickness,
-      double? highlightedSize}) {
+      double? highlightedThickness =
+          _GroovedDividerPainter1.defaultHighlightedThickness,
+      double? highlightedSize =
+          _GroovedDividerPainter1.defaultHighlightedSize}) {
     return _GroovedDividerPainter1(
         size: size,
         backgroundColor: backgroundColor,
@@ -68,6 +70,34 @@ class DividerPainters {
         thickness: thickness,
         highlightedThickness: highlightedThickness,
         highlightedSize: highlightedSize);
+  }
+
+  /// Builds a grooved divider painter.
+  static DividerPainter grooved2(
+      {double size = _GroovedDividerPainter2.defaultSize,
+      Color? backgroundColor,
+      Color? highlightedBackgroundColor,
+      bool animationEnabled = DividerPainter.defaultAnimationEnabled,
+      Color color = _GroovedDividerPainter2.defaultColor,
+      double gap = _GroovedDividerPainter2.defaultGap,
+      int count = _GroovedDividerPainter2.defaultCount,
+      Color? highlightedColor = _GroovedDividerPainter2.defaultHighlightedColor,
+      StrokeCap strokeCap = _GroovedDividerPainter2.defaultStrokeCap,
+      double thickness = _GroovedDividerPainter2.defaultThickness,
+      int? highlightedCount =
+          _GroovedDividerPainter2.defaultHighlightedCount}) {
+    return _GroovedDividerPainter2(
+        size: size,
+        backgroundColor: backgroundColor,
+        highlightedBackgroundColor: highlightedBackgroundColor,
+        animationEnabled: animationEnabled,
+        color: color,
+        gap: gap,
+        count: count,
+        highlightedColor: highlightedColor,
+        strokeCap: strokeCap,
+        thickness: thickness,
+        highlightedCount: highlightedCount);
   }
 }
 
@@ -263,9 +293,12 @@ class _GroovedDividerPainter1 extends DividerPainter {
   static const int sizeKey = 3;
 
   static const double defaultSize = 25;
+  static const double defaultHighlightedSize = 40;
   static const double defaultThickness = 2;
+  static const double defaultHighlightedThickness = 4;
   static const StrokeCap defaultStrokeCap = StrokeCap.round;
-  static const Color defaultColor = Colors.black;
+  static const Color defaultColor = Colors.black38;
+  static const Color defaultHighlightedColor = Colors.black;
 
   _GroovedDividerPainter1(
       {this.size = defaultSize,
@@ -273,11 +306,11 @@ class _GroovedDividerPainter1 extends DividerPainter {
       Color? highlightedBackgroundColor,
       bool animationEnabled = DividerPainter.defaultAnimationEnabled,
       this.color = defaultColor,
-      this.highlightedColor,
+      this.highlightedColor = defaultHighlightedColor,
       this.strokeCap = defaultStrokeCap,
       this.thickness = defaultThickness,
-      this.highlightedThickness,
-      this.highlightedSize})
+      this.highlightedThickness = defaultHighlightedThickness,
+      this.highlightedSize = defaultHighlightedSize})
       : super(
             animationEnabled: animationEnabled,
             backgroundColor: backgroundColor,
@@ -374,6 +407,140 @@ class _GroovedDividerPainter1 extends DividerPainter {
       }
       if (highlightedSize != null) {
         map[sizeKey] = Tween<double>(begin: size, end: highlightedSize);
+      }
+    }
+    return map;
+  }
+}
+
+/// Divider with grooves (style 2).
+class _GroovedDividerPainter2 extends DividerPainter {
+  static const int colorKey = 1;
+  static const int countKey = 2;
+
+  static const double defaultSize = 4;
+  static const double defaultThickness = 2;
+  static const double defaultGap = 5;
+  static const int defaultCount = 4;
+  static const StrokeCap defaultStrokeCap = StrokeCap.round;
+  static const Color defaultColor = Colors.black38;
+  static const Color defaultHighlightedColor = Colors.black;
+  static const int defaultHighlightedCount = 7;
+
+  _GroovedDividerPainter2(
+      {this.size = defaultSize,
+      Color? backgroundColor,
+      Color? highlightedBackgroundColor,
+      bool animationEnabled = DividerPainter.defaultAnimationEnabled,
+      this.color = defaultColor,
+      this.gap = defaultGap,
+      this.count = defaultCount,
+      this.highlightedColor = defaultHighlightedColor,
+      this.strokeCap = defaultStrokeCap,
+      this.thickness = defaultThickness,
+      this.highlightedCount = defaultHighlightedCount})
+      : super(
+            animationEnabled: animationEnabled,
+            backgroundColor: backgroundColor,
+            highlightedBackgroundColor: highlightedBackgroundColor) {
+    if (size <= 0) {
+      throw Exception('The size parameter must be positive: $size');
+    }
+    if (gap <= 0) {
+      throw Exception('The gap parameter must be positive: $gap');
+    }
+    if (count < 1) {
+      throw Exception('The count parameter must be bigger than 1: $count');
+    }
+    if (thickness <= 0) {
+      throw Exception('The thickness parameter must be positive: $thickness');
+    }
+    if (highlightedCount != null && highlightedCount! < 1) {
+      throw Exception(
+          'The highlightedCount parameter must be bigger than 1: $highlightedCount');
+    }
+  }
+
+  final double size;
+  final double gap;
+  final int count;
+  final int? highlightedCount;
+  final Color color;
+  final Color? highlightedColor;
+  final StrokeCap strokeCap;
+  final double thickness;
+
+  @override
+  void paint(
+      {required Axis dividerAxis,
+      required bool resizable,
+      required bool highlighted,
+      required Canvas canvas,
+      required Size dividerSize,
+      required Map<int, dynamic> animatedValues}) {
+    super.paint(
+        dividerAxis: dividerAxis,
+        resizable: resizable,
+        highlighted: highlighted,
+        canvas: canvas,
+        dividerSize: dividerSize,
+        animatedValues: animatedValues);
+    Color? _color;
+    if (animationEnabled && animatedValues.containsKey(colorKey)) {
+      _color = animatedValues[colorKey];
+    } else if (highlighted && highlightedColor != null) {
+      _color = highlightedColor!;
+    } else {
+      _color = color;
+    }
+
+    if (_color != null) {
+      int _count = count;
+      if (animationEnabled && animatedValues.containsKey(countKey)) {
+        _count = animatedValues[countKey];
+      } else if (highlighted && highlightedCount != null) {
+        _count = highlightedCount!;
+      }
+
+      var paint = Paint()
+        ..style = PaintingStyle.stroke
+        ..color = _color
+        ..strokeWidth = thickness
+        ..strokeCap = strokeCap
+        ..isAntiAlias = true;
+      if (dividerAxis == Axis.vertical) {
+        double startY = dividerSize.height / 2;
+        double x = (dividerSize.width - size) / 2;
+        canvas.drawLine(Offset(x, startY), Offset(x + size, startY), paint);
+        for (int i = 1; i < _count; i++) {
+          canvas.drawLine(Offset(x, startY + (gap * i)),
+              Offset(x + size, startY + (gap * i)), paint);
+          canvas.drawLine(Offset(x, startY - (gap * i)),
+              Offset(x + size, startY - (gap * i)), paint);
+        }
+      } else {
+        double startX = dividerSize.width / 2;
+        double y = (dividerSize.height - size) / 2;
+        canvas.drawLine(Offset(startX, y), Offset(startX, y + size), paint);
+        for (int i = 1; i < _count; i++) {
+          canvas.drawLine(Offset(startX + (gap * i), y),
+              Offset(startX + (gap * i), y + size), paint);
+          canvas.drawLine(Offset(startX - (gap * i), y),
+              Offset(startX - (gap * i), y + size), paint);
+        }
+      }
+    }
+  }
+
+  @override
+  Map<int, Tween> buildTween() {
+    Map<int, Tween> map = super.buildTween();
+    if (animationEnabled) {
+      if (highlightedColor != null) {
+        map[colorKey] = ColorTween(begin: color, end: highlightedColor);
+      }
+      if (highlightedCount != null) {
+        map[countKey] = IntTween(begin: count, end: highlightedCount);
       }
     }
     return map;
