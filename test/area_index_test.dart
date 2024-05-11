52d025de03f0e30cedf3982102e6a9a350245d9f
Carlos Eduardo Leite de Andrade <cadu@caduandrade.net>
2024-05-11 15:09:38 -0300

    Removing unnecessary import

--- test/area_index_test.dart
+++ test/area_index_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:multi_split_view/multi_split_view.dart';
-import 'package:multi_split_view/src/area.dart';
 
 void main() {
   group('Area', () {
